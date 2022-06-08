87a8ddf72430d1f8a913321a89177daeddcd677e
danapt <apetridaniel3@gmail.com>
2022-06-09 00:34:20 +0200

    need to fix last 2 tests!

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -18,6 +18,39 @@ class Blockchain:
     def __repr__(self):
         return f'Blockchain : {self.chain}'
 
+    def replace_chain(self, chain):
+        """
+        Replace the local chain with incoming one if the following applies:
+        - the incoming chain is longer than the local one.
+        - the incoming chain is formatted properly
+        """
+        if len(chain) <= len(self.chain):
+             raise Exception('Cannot replace. the incoming chain must be longer.')
+
+        try:
+            Blockchain.is_valid_chain(chain)
+        except Exception as e:
+            raise Exception(f'Can not replace. the incoming chain is invalid: {e}')
+
+        self.chain = chain
+
+    @staticmethod
+    def is_valid_chain(chain):
+        """
+        Validate the incoming chain.
+        Enforce the following rules of blockchain:
+            - teh cain must start with genesis block
+            - blocks must be formatted correctly
+        """
+        if chain[0] != Block.genesis():
+            raise  Exception('the genesis block must be valid')
+        for i in range(1,len(chain)):
+            block = chain[i]
+            last_block = chain[i-1]
+            Block.is_valid_block(last_block,block)
+
+
+
 
 def main():
     blockchain = Blockchain()
